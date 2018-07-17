bcd3356a8c00725e671271bfeb781bb126037724
shuxuannie <judy.nie@oracle.com>
2018-07-16 20:04:35 -0700

    4 Sum

---
+++ src/test/study/algorithm/Array/FourSum_18Test.java
@@ -0,0 +1,28 @@
+package test.study.algorithm.Array;
+
+import main.study.algorithm.Array.FourSum_18;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FourSum_18Test {
+
+    @Test
+    void fourSum() {
+        int[] nums = {1, 0, -1, 0, -2, 2};
+        int target = 0;
+
+        FourSum_18 fourSum_18 = new FourSum_18();
+        List<List<Integer>> rst = fourSum_18.fourSum(nums, target);
+
+        assertEquals(3, rst.size());
+
+        assertEquals(Arrays.asList(-2,-1,1,2), rst.get(0));
+        assertEquals(Arrays.asList(-2,0,0,2), rst.get(1));
+        assertEquals(Arrays.asList(-1,0,0,1), rst.get(2));
+    }
+}

bb0f1f02728a4be0aac7b785ed170fdfff590699
Shuxuan <judy.nie@oracle.com>
2018-07-26 09:20:04 -0700

    merge interval

---
+++ src/main/study/algorithm/Array/MergeIntervals_56.java
@@ -0,0 +1,29 @@
+package main.study.algorithm.Array;
+
+import sun.jvm.hotspot.utilities.Interval;
+
+import java.util.List;
+
+/**
+ *
+ * https://leetcode.com/problems/merge-intervals/description/
+ *
+ * Given a collection of intervals, merge all overlapping intervals.
+ *
+ * Example 1:
+ *
+ * Input: [[1,3],[2,6],[8,10],[15,18]]
+ * Output: [[1,6],[8,10],[15,18]]
+ * Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+ * Example 2:
+ *
+ * Input: [[1,4],[4,5]]
+ * Output: [[1,5]]
+ * Explanation: Intervals [1,4] and [4,5] are considerred overlapping.
+ */
+public class MergeIntervals_56 {
+
+    public List<Interval> merge(List<Interval> intervals) {
+
+    }
+}

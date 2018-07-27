05335b2a4b6f2a3c5529c4d5efb6b25ea7ae3a51
shuxuannie <shuxuan.nie@gmail.com>
2018-07-26 22:10:00 -0700

    unique path

--- src/main/study/algorithm/Array/UniquePaths_62.java
+++ src/main/study/algorithm/Array/UniquePaths_62.java
@@ -37,22 +37,19 @@ public class UniquePaths_62 {
             return 0;
         }
 
-        int[][] status = new int[m][n];
+        int[] status = new int[n];
 
-        for (int i = 0; i < m; i++) {
-            status[i][0] = 1;
+        for (int i = 0; i < n; i++) {
+            status[i] = 1;
         }
 
-        for (int j = 0; j < n; j++) {
-            status[0][j] = 1;
-        }
 
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                status[i][j] = status[i-1][j] + status[i][j-1];
+                status[j] = status[j] + status[j-1];
             }
         }
-        return status[m-1][n-1];
+        return status[n-1];
     }
 
     public static void main(String[] args) {

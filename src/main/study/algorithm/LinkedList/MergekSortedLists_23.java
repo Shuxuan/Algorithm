3de4f5bf5dd9109d92c50ccb1053b0188c70a0c5
shuxuannie <shuxuan.nie@gmail.com>
2018-07-31 16:47:52 -0700

    merge k sorted linkedlist

--- src/main/study/algorithm/LinkedList/MergekSortedLists_23.java
+++ src/main/study/algorithm/LinkedList/MergekSortedLists_23.java
@@ -18,7 +18,58 @@ package main.study.algorithm.LinkedList;
 public class MergekSortedLists_23 {
 
     public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+
+        if (lists.length == 1) {
+            return lists[0];
+        }
+
+        return helper(lists, 0, lists.length-1);
 
     }
-    
+
+    public ListNode helper(ListNode[] lists, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            return mergeTwoLists(helper(lists, left, mid), helper(lists, mid+1, right));
+        }
+
+        return lists[left];
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return l1;
+        }
+
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode helper = new ListNode(0);
+        helper.next = l1;
+        ListNode pre = helper;
+
+        //以L1为基准，比L1小的，插入Pre和L1之间，否则L1后移
+        while (l1 != null && l2 != null) {
+            if (l2.val < l1.val) {
+                //insert l2 between pre and l1
+                ListNode next = l2.next;
+                l2.next = pre.next;
+                pre.next = l2;
+                l2 = next;
+            } else {
+                l1 = l1.next;
+            }
+
+            pre = pre.next;
+        }
+
+        if (l2 != null) {
+            pre.next = l2;
+        }
+
+        return helper.next;
+    }
 }

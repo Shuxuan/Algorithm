137489637743eaf5b1516ee0dfa362d0a79f95b3
shuxuannie <shuxuan.nie@gmail.com>
2018-08-03 12:55:28 -0700

    reverse linked list

---
+++ src/main/study/algorithm/LinkedList/ReverseLinkedList_206.java
@@ -0,0 +1,35 @@
+package main.study.algorithm.LinkedList;
+
+/**
+ * https://leetcode.com/problems/reverse-linked-list/description/
+ *
+ * Reverse a singly linked list.
+ *
+ * Example:
+ *
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
+ * Follow up:
+ *
+ * A linked list can be reversed either iteratively or recursively. Could you implement both?
+ */
+public class ReverseLinkedList_206 {
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        return pre;
+
+    }
+}

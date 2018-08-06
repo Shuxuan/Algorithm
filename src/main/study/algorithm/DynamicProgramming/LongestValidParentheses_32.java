97cff035c14351780afd9859341c8953bd0b9bdf
shuxuannie <shuxuan.nie@gmail.com>
2018-08-05 23:37:46 -0700

    longest valid parentheses

--- src/main/study/algorithm/DynamicProgramming/LongestValidParentheses_32.java
+++ src/main/study/algorithm/DynamicProgramming/LongestValidParentheses_32.java
@@ -39,6 +39,8 @@ public class LongestValidParentheses_32 {
         boolean[][] f = new boolean[n][n];
         for (int j = 1; j < n; j++) {
             for (int i = 0; i + j < n; i++) {
+                if (s.charAt(i+j) == '(')
+                    continue;
                 if (j == 1 ){
                    if(s.charAt(i) == '(' && s.charAt(i+j) == ')') {
                         f[i][i+j] = true;
---
+++ src/test/study/algorithm/DynamicProgramming/LongestValidParentheses_32Test.java
@@ -0,0 +1,69 @@
+package test.study.algorithm.DynamicProgramming;
+
+import main.study.algorithm.DynamicProgramming.LongestValidParentheses_32;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LongestValidParentheses_32Test {
+
+    @Test
+    void longestValidParentheses() {
+        String s = "(()";
+        LongestValidParentheses_32 myObj = new LongestValidParentheses_32();
+        int rst = myObj.longestValidParentheses(s);
+
+        System.out.println(rst);
+        assertEquals(2, rst);
+    }
+
+    @Test
+    void longestValidParentheses2() {
+        String s = ")()())";
+        LongestValidParentheses_32 myObj = new LongestValidParentheses_32();
+        int rst = myObj.longestValidParentheses(s);
+
+        System.out.println(rst);
+        assertEquals(4, rst);
+    }
+
+    @Test
+    void longestValidParentheses3() {
+        String s = "()(())";
+        LongestValidParentheses_32 myObj = new LongestValidParentheses_32();
+        int rst = myObj.longestValidParentheses(s);
+
+        System.out.println(rst);
+        assertEquals(6, rst);
+    }
+
+    @Test
+    void longestValidParentheses4() {
+        String s = "(())";
+        LongestValidParentheses_32 myObj = new LongestValidParentheses_32();
+        int rst = myObj.longestValidParentheses(s);
+
+        System.out.println(rst);
+        assertEquals(4, rst);
+    }
+
+    @Test
+    void longestValidParentheses5() {
+        String s = "()()";
+        LongestValidParentheses_32 myObj = new LongestValidParentheses_32();
+        int rst = myObj.longestValidParentheses(s);
+
+        System.out.println(rst);
+        assertEquals(4, rst);
+    }
+
+    @Test
+    void longestValidParentheses6() {
+        String s = ")(((((()())()()))()(()))(";
+        LongestValidParentheses_32 myObj = new LongestValidParentheses_32();
+        int rst = myObj.longestValidParentheses(s);
+
+        System.out.println(rst);
+        assertEquals(22, rst);
+    }
+}

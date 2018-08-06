5a4f9eb8f92bed7bbecfd5a005b581ff2d6c4646
shuxuannie <shuxuan.nie@gmail.com>
2018-08-05 23:33:40 -0700

    longest valid parentheses

---
+++ src/main/study/algorithm/DynamicProgramming/LongestValidParentheses_32.java
@@ -0,0 +1,78 @@
+package main.study.algorithm.DynamicProgramming;
+
+/**
+ * https://leetcode.com/problems/longest-valid-parentheses/description/
+ *
+ * Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+ *
+ * Example 1:
+ *
+ * Input: "(()"
+ * Output: 2
+ * Explanation: The longest valid parentheses substring is "()"
+ * Example 2:
+ *
+ * Input: ")()())"
+ * Output: 4
+ * Explanation: The longest valid parentheses substring is "()()"
+ */
+public class LongestValidParentheses_32 {
+
+    public int longestValidParentheses(String s) {
+
+        if (s == null || s.isEmpty() || s.length() == 1) {
+            return 0;
+        }
+
+        int max = 0;
+
+        int n = s.length();
+
+        /**
+         * f[i][j]为true的情况是3种：
+         * * ([True]) 头尾字符是对称的括号，中间也是
+         * [True]() 结尾俩个是对称的括号，前面也是
+         * ()[True] 头部俩个是堆成的括号，后面也是
+         *
+         */
+
+        boolean[][] f = new boolean[n][n];
+        for (int j = 1; j < n; j++) {
+            for (int i = 0; i + j < n; i++) {
+                if (j == 1 ){
+                   if(s.charAt(i) == '(' && s.charAt(i+j) == ')') {
+                        f[i][i+j] = true;
+                }
+
+                } else {
+                    if (f[i][i+1] && f[i+2][i+j]) {
+                        f[i][i+j] = true;
+                    }
+                    if (f[i][i+j-2] && f[i+j-1][i+j]) {
+                        f[i][i+j] = true;
+                    }
+                    if (f[i+1][i+j-1] && s.charAt(i) == '(' && s.charAt(i+j) == ')') {
+                        f[i][i+j] = true;
+                    }
+
+                }
+
+                if (f[i][i+j]) {
+                    max = Math.max(max, j+1);
+                }
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        String s = "(()";
+        LongestValidParentheses_32 myObj = new LongestValidParentheses_32();
+        int rst = myObj.longestValidParentheses(s);
+
+        System.out.println(rst);
+
+    }
+
+}

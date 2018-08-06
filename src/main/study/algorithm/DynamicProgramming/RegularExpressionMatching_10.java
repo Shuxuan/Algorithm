160000706b21bba716d2f283993fa9c1894630fd
Shuxuan <judy.nie@oracle.com>
2018-08-06 06:19:11 -0700

    regular expression match

--- src/main/study/algorithm/DynamicProgramming/RegularExpressionMatching_10.java
+++ src/main/study/algorithm/DynamicProgramming/RegularExpressionMatching_10.java
@@ -63,52 +63,37 @@ public class RegularExpressionMatching_10 {
          *
          * else f[i][j] = false (default is false)
          */
-        if (s == null || p == null) {
-            return false;
+        if (s == null && p == null) {
+            return true;
         }
 
-        int m = s.length();
-        int n = p.length();
-        boolean[][] f = new boolean[m+1][n+1];
-        f[0][0] = true;
 
 
-        /**
-         * f[i][0] = false (i >= 1)
-         * f[0][j] = false (j >= 1), if pattern is like a*b*c*, f[0][j] = true;
-         */
+        int len_s = s.length();
+        int len_p = p.length();
+        boolean[][] dp = new boolean[len_s+1][len_p+1];
+        dp[0][0] = true;
 
-        if (p.length() > 0 && p.length() % 2 == 0) {
-            boolean starFlag = true;
-            for (int i = 1; i < n+1; i = i + 2) {
-                if (p.charAt(i) != '*')  {
-                    starFlag = false;
-                    break;
-                }
-            }
-
-            if (starFlag) {
-                for (int i = 1; i < n+1; i++) {
-                    f[0][i] = true;
-                }
+        for (int i = 2; i <= len_p; i++) {
+            if (p.charAt(i-1) == '*') {
+                dp[0][i] = dp[0][i-2];
             }
         }
 
-        for (int i = 1; i < m + 1; i++) {
-            for (int j = 1; j < n + 1; j++) {
+        for (int i = 1; i <= len_s; i++) {
+            for (int j = 1; j <= len_p; j++) {
                 if (s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '.') {
-                    f[i][j] = f[i-1][j-1];
+                    dp[i][j] = dp[i-1][j-1];
                 } else if (p.charAt(j-1) == '*') {
+                    dp[i][j] = dp[i][j-2];
                     if (s.charAt(i-1) == p.charAt(j-2) || p.charAt(j-2) == '.') {
-                        f[i][j] = f[i-1][j];
-                    } else {
-                        f[i][j] = f[i][j-2];
+                        dp[i][j] = dp[i][j] || dp[i-1][j];
                     }
                 }
             }
         }
 
-        return f[m][n];
+        return dp[len_s][len_p];
 
     }
 

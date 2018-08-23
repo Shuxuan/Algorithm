5b1a759fb88def321368564d5f29944b46cb9761
shuxuannie <shuxuan.nie@gmail.com>
2018-08-23 14:58:44 -0700

    Symmetric Tree

---
+++ src/main/study/algorithm/Tree/SymmetricTree_101.java
@@ -0,0 +1,74 @@
+package main.study.algorithm.Tree;
+
+import java.util.Stack;
+
+/**
+ * https://leetcode.com/problems/symmetric-tree/description/
+ *
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+ *
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ * But the following [1,2,2,null,3,null,3] is not:
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ * Note:
+ * Bonus points if you could solve it both recursively and iteratively.
+ */
+public class SymmetricTree_101 {
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null)
+            return true;
+        Stack stack = new Stack();
+        stack.push(root.left);
+        stack.push(root.right);
+        while (!stack.isEmpty()) {
+            TreeNode node1 = (TreeNode) stack.pop();
+            TreeNode node2 = (TreeNode) stack.pop();
+            if (node1 == null && node2 == null)
+                continue;
+            if (node1 == null || node2 == null)
+                return false;
+            if (node1.val != node2.val)
+                return false;
+            stack.push(node1.left);
+            stack.push(node2.right);
+            stack.push(node1.right);
+            stack.push(node2.left);
+        }
+        return true;
+    }
+
+    public boolean isSymmetric_recursion(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return helper(root.left, root.right);
+    }
+
+    private boolean helper(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left == null || right == null) {
+            return false;
+        }
+
+        if (left.val != right.val) {
+            return false;
+        }
+
+        return helper(left.left, right.right) && helper(left.right, right.left);
+    }
+
+}

a74578d0efcac397b1ac24baba18c2805f5cf197
shuxuannie <shuxuan.nie@gmail.com>
2018-08-30 09:59:48 -0700

    count complete tree nodes

--- src/main/study/algorithm/Tree/CountCompleteTreeNodes_222.java
+++ src/main/study/algorithm/Tree/CountCompleteTreeNodes_222.java
@@ -1,5 +1,7 @@
 package main.study.algorithm.Tree;
 
+import static java.lang.Math.pow;
+
 /**
  * https://leetcode.com/problems/count-complete-tree-nodes/description/
  *
@@ -23,12 +25,45 @@ package main.study.algorithm.Tree;
  */
 public class CountCompleteTreeNodes_222 {
 
+    /**
+     * 2的h次方-1，h为该完美二叉树的高度。这道题可以用递归和非递归两种方法来解。
+     * 我们先来看递归的方法，思路是分别找出以当前节点为根节点的左子树和右子树的高度并对比，
+     * 如果相等，则说明是满二叉树，直接返回节点个数，
+     * 如果不相等，则节点个数为左子树的节点个数加上右子树的节点个数再加1(根节点)，
+     * 其中左右子树节点个数的计算可以使用递归来计算，
+     *
+     * @param root
+     * @return
+     */
     public int countNodes(TreeNode root) {
-        if (root == null) {
-            return 0;
+        int hLeft = leftHeight(root);
+        int hRight = rightHeight(root);
+
+        if (hLeft == hRight) {
+            return (int)Math.pow(2, hLeft) - 1;
+        } else {
+            return 1 + countNodes(root.left) + countNodes(root.right);
+        }
+    }
+
+    private int leftHeight(TreeNode root) {
+        int h = 0;
+        while (root != null) {
+            h++;
+            root = root.left;
         }
 
-        
-
+        return h;
     }
+
+    private int rightHeight(TreeNode root) {
+        int h = 0;
+        while (root != null) {
+            h++;
+            root = root.right;
+        }
+
+        return h;
+    }
+
 }

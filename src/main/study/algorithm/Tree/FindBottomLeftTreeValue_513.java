bd405f80f3bb3884d39b2a1c6d06cdbaad7ebdef
shuxuannie <shuxuan.nie@gmail.com>
2018-09-14 11:29:39 -0700

    tree

---
+++ src/main/study/algorithm/Tree/FindBottomLeftTreeValue_513.java
@@ -0,0 +1,42 @@
+package main.study.algorithm.Tree;
+
+/**
+ * https://leetcode.com/problems/find-bottom-left-tree-value/description/
+ *
+ * Given a binary tree, find the leftmost value in the last row of the tree.
+ *
+ * Example 1:
+ * Input:
+ *
+ *     2
+ *    / \
+ *   1   3
+ *
+ * Output:
+ * 1
+ * Example 2:
+ * Input:
+ *
+ *         1
+ *        / \
+ *       2   3
+ *      /   / \
+ *     4   5   6
+ *        /
+ *       7
+ *
+ * Output:
+ * 7
+ */
+public class FindBottomLeftTreeValue_513 {
+
+    /**
+     * level order traverse the tree, last level, the first element is the answer
+     * @param root
+     * @return
+     */
+    public int findBottomLeftValue(TreeNode root) {
+
+
+    }
+}

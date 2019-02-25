28ff1dcae9aec01b1954ef8c265110fc2591c6e7
shuxuannie <shuxuan.nie@gmail.com>
2019-02-25 07:53:34 -0800

    fb

---
+++ src/main/company/facebook/WordBreak.java
@@ -0,0 +1,32 @@
+package main.company.facebook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordBreak {
+
+
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if (s.isEmpty()) return true;
+        for(String word : wordDict) {
+            if (s.startsWith(word) && wordBreak(s.substring(word.length()), wordDict))
+                return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        List<String> wordDict = new ArrayList<>();
+        wordDict.add("leet");
+        wordDict.add("code");
+
+        String s = "leetcode";
+
+        WordBreak myObj = new WordBreak();
+        System.out.println(myObj.wordBreak(s, wordDict));
+
+
+    }
+}

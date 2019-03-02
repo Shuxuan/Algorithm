9f0e322ca31246b9df86ca3cfe4eb1164208bafd
shuxuannie <shuxuan.nie@gmail.com>
2019-03-02 11:39:43 -0800

    fb

---
+++ src/main/company/vmware/BSTIterator.java
@@ -0,0 +1,47 @@
+package main.company.vmware;
+
+import main.study.algorithm.Tree.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class BSTIterator {
+    Stack<TreeNode> stack;
+    LinkedList<Integer> list;
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        list = new LinkedList<>();
+        inorder(root);
+
+    }
+
+    private void inorder(TreeNode root) {
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+            list.add(root.val);
+            root = root.right;
+        }
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        int val = list.getFirst();
+        list.removeFirst();
+        return val;
+
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        if (list.size() > 0) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/main/company/vmware/LowestCommonAncestorofaBinaryTree.java
@@ -0,0 +1,37 @@
+package main.company.vmware;
+
+import main.study.algorithm.Tree.TreeNode;
+
+public class LowestCommonAncestorofaBinaryTree {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        TreeNode rst = helper(root, p, q);
+
+        return rst;
+    }
+
+    private TreeNode helper(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode l = helper(root.left, p, q);
+        TreeNode r = helper(root.right, p, q);
+
+        if (l != null && r != null) {
+            return root;
+        }
+
+        if (l == null) {
+            return r;
+        }
+
+        return l;
+
+    }
+
+}
